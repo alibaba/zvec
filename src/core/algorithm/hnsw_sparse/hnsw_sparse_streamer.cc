bc33e81536a3414310edb6006a5238128a2a0c14
zhourrr <zhouqinren@foxmail.com>
2026-03-16 15:41:21 +0800

    fix: revise

--- src/core/algorithm/hnsw_sparse/hnsw_sparse_streamer.cc
+++ src/core/algorithm/hnsw_sparse/hnsw_sparse_streamer.cc
@@ -525,7 +525,7 @@ int HnswSparseStreamer::add_impl(uint64_t pkey, const uint32_t sparse_count,
     return ret;
   }
 
-  if (ailego_unlikely(sparse_count >= HnswSparseEntity::kSparseMaxDimSize)) {
+  if (ailego_unlikely(sparse_count > HnswSparseEntity::kSparseMaxDimSize)) {
     LOG_WARN(
         "Failed to add sparse vector: number of non-zero elements (%u) exceeds "
         "maximum allowed (%u), key=%zu",
