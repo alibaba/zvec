037638cf71f905baa8ca2cf5f74a91f172498a47
zhourrr <zhouqinren@foxmail.com>
2026-03-18 14:47:19 +0800

    feat: add ut

--- tests/db/crash_recovery/write_recovery_test.cc
+++ tests/db/crash_recovery/write_recovery_test.cc
@@ -27,8 +27,8 @@ namespace zvec {
 
 
 static std::string data_generator_bin_;
-const std::string collection_name_{"crash_test"};
-const std::string dir_path_{"crash_test_db"};
+const std::string collection_name_{"write_recovery_test"};
+const std::string dir_path_{"write_recovery_test_db"};
 const zvec::CollectionOptions options_{false, true};
 
 
