05e3dcfe2fdda08fff5cc4add35b35b0b8139fca
ray <rui.xing@alibaba-inc.com>
2026-03-20 22:24:59 +0800

    fix: clang format

--- src/ailego/math_batch/euclidean_distance_batch_impl_fp16_avx512.cc
+++ src/ailego/math_batch/euclidean_distance_batch_impl_fp16_avx512.cc
@@ -127,11 +127,11 @@ void compute_one_to_many_squared_euclidean_avx512f_fp16_12(
     const ailego::Float16 *query, const ailego::Float16 **ptrs,
     std::array<const ailego::Float16 *, 12> &prefetch_ptrs, size_t dim,
     float *sums) {
-  return compute_one_to_many_squared_euclidean_avx512f_fp16<ailego::Float16, 12>(
+  return compute_one_to_many_squared_euclidean_avx512f_fp16<ailego::Float16,
+                                                            12>(
       query, ptrs, prefetch_ptrs, dim, sums);
 }
 
 #endif
-
 
 }  // namespace zvec::ailego::DistanceBatch
