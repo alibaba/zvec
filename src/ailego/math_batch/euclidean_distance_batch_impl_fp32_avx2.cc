91e9813707d11db522ca9fe03efea8f41075cd9b
raymond <rui.xing@alibaba-inc.com>
2026-03-20 22:49:05 +0800

    fix: fix format

--- src/ailego/math_batch/euclidean_distance_batch_impl_fp32_avx2.cc
+++ src/ailego/math_batch/euclidean_distance_batch_impl_fp32_avx2.cc
@@ -62,25 +62,25 @@ compute_one_to_many_squared_euclidean_avx2_fp32(
 
     switch (dimensionality - dim) {
       case 7:
-        SSD_FP32_GENERAL(query[dim+6], ptrs[i][dim+6], results[i]);
+        SSD_FP32_GENERAL(query[dim + 6], ptrs[i][dim + 6], results[i]);
         /* FALLTHRU */
       case 6:
-        SSD_FP32_GENERAL(query[dim+5], ptrs[i][dim+5], results[i]);
+        SSD_FP32_GENERAL(query[dim + 5], ptrs[i][dim + 5], results[i]);
         /* FALLTHRU */
       case 5:
-        SSD_FP32_GENERAL(query[dim+4], ptrs[i][dim+4], results[i]);
+        SSD_FP32_GENERAL(query[dim + 4], ptrs[i][dim + 4], results[i]);
         /* FALLTHRU */
       case 4:
-        SSD_FP32_GENERAL(query[dim+3], ptrs[i]dim+[3], results[i]);
+        SSD_FP32_GENERAL(query[dim + 3], ptrs[i] dim + [3], results[i]);
         /* FALLTHRU */
       case 3:
-        SSD_FP32_GENERAL(query[dim+2], ptrs[i][dim+2], results[i]);
+        SSD_FP32_GENERAL(query[dim + 2], ptrs[i][dim + 2], results[i]);
         /* FALLTHRU */
       case 2:
-        SSD_FP32_GENERAL(query[dim+1], ptrs[i][dim+1], results[i]);
+        SSD_FP32_GENERAL(query[dim + 1], ptrs[i][dim + 1], results[i]);
         /* FALLTHRU */
       case 1:
-        SSD_FP32_GENERAL(query[dim+0], ptrs[i][dim+0], results[i]);
+        SSD_FP32_GENERAL(query[dim + 0], ptrs[i][dim + 0], results[i]);
     }
   }
 }
