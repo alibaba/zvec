6e814a208d846fba54427303a6d71a1dd4061a2b
zhourrr <zhouqinren@foxmail.com>
2026-03-23 11:19:35 +0800

    fix: ut

--- tests/db/crash_recovery/optimize_recovery_test.cc
+++ tests/db/crash_recovery/optimize_recovery_test.cc
@@ -31,7 +31,7 @@ const std::string collection_name_{"optimize_recovery_test"};
 const std::string dir_path_{"optimize_recovery_test_db"};
 const zvec::CollectionOptions options_{false, true, 256 * 1024};
 const int batch_size{50};
-const int num_batches{2000};
+const int num_batches{1000};
 
 
 static std::string LocateOptimizeGenerator() {
@@ -157,8 +157,20 @@ TEST_F(OptimizeRecoveryTest, CrashDuringOptimize) {
       }
     }
 
+    VectorQuery query;
+    query.topk_ = 10;
+    std::vector<float> feature(128, 0.0);
+    query.query_vector_.assign((const char *)feature.data(),
+                               feature.size() * sizeof(float));
+    query.field_name_ = "dense_fp32_field";
+    auto query_result = collection->Query(query);
+    ASSERT_TRUE(query_result);
+    auto doc_list = query_result.value();
+    ASSERT_EQ(doc_list.size(), 10);
+    ASSERT_EQ(doc_list[0]->pk(), "pk_0");
+
     // Insert some more documents
-    for (int batch = num_batches; batch < num_batches + 1000; batch++) {
+    for (int batch = num_batches; batch < num_batches + 500; batch++) {
       std::vector<Doc> docs;
       for (int i = 0; i < batch_size; i++) {
         docs.push_back(CreateTestDoc(batch * batch_size + i, 0));
@@ -181,29 +193,36 @@ TEST_F(OptimizeRecoveryTest, CrashDuringOptimize) {
                                      "Recovery mechanism may be broken.";
   auto collection = result.value();
   uint64_t doc_count{collection->Stats().value().doc_count};
-  ASSERT_EQ(doc_count, (num_batches + 1000) * batch_size);
+  ASSERT_EQ(doc_count, (num_batches + 500) * batch_size);
+  for (uint64_t doc_id = 0; doc_id < doc_count; doc_id++) {
+    Doc expected_doc = CreateTestDoc(doc_id, 0);
+    std::vector<std::string> pks{};
+    pks.emplace_back(expected_doc.pk());
+    if (auto res = collection->Fetch(pks); res) {
+      auto map = res.value();
+      if (map.find(expected_doc.pk()) == map.end()) {
+        FAIL() << "Returned map does not contain doc[" << expected_doc.pk()
+               << "]";
+      }
+      const auto actual_doc = map.at(expected_doc.pk());
+      ASSERT_EQ(*actual_doc, expected_doc)
+          << "Data mismatch for doc[" << expected_doc.pk() << "]";
+    } else {
+      FAIL() << "Failed to fetch doc[" << expected_doc.pk() << "]";
+    }
+  }
 
-  // for (int batch = num_batches; batch < (num_batches + 1000); batch++) {
-  //   std::vector<Doc> docs;
-  //   for (int i = 0; i < batch_size; i++) {
-  //     docs.push_back(CreateTestDoc(batch * batch_size + i, 0));
-  //   }
-  //   auto write_result = collection->Insert(docs);
-  //   ASSERT_TRUE(write_result);
-  //   for (auto &s : write_result.value()) {
-  //     ASSERT_TRUE(s.ok()) << s.message();
-  //   }
-  // }
-
-  // VectorQuery query;
-  // query.output_fields_ = {"name", "age"};
-  // query.filter_ = "invert_id >= 6000 and id < 6080";
-  // query.topk_ = 100;
-  // std::vector<float> feature(4, 0.0);
-  // query.query_vector_.assign((const char *)feature.data(),
-  //                            feature.size() * sizeof(float));
-  // query.field_name_ = "dense";
-  // collection->Query();
+  VectorQuery query;
+  query.topk_ = 10;
+  std::vector<float> feature(128, 0.0);
+  query.query_vector_.assign((const char *)feature.data(),
+                             feature.size() * sizeof(float));
+  query.field_name_ = "dense_fp32_field";
+  auto query_result = collection->Query(query);
+  ASSERT_TRUE(query_result);
+  auto doc_list = query_result.value();
+  ASSERT_EQ(doc_list.size(), 10);
+  ASSERT_EQ(doc_list[0]->pk(), "pk_0");
 }
 
 
