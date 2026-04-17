ce8490b4efa47f63ed67783efd3d047905c9e112
Qinren Zhou <zhouqinren.zqr@alibaba-inc.com>
2026-04-17 13:38:09 +0800

    fix

--- tests/db/collection_test.cc
+++ tests/db/collection_test.cc
@@ -30,7 +30,6 @@
 #include "db/index/common/type_helper.h"
 #include "index/utils/utils.h"
 #include "zvec/ailego/utility/float_helper.h"
-#include "zvec/db/config.h"
 #include "zvec/db/doc.h"
 #include "zvec/db/index_params.h"
 #include "zvec/db/options.h"
@@ -3236,7 +3235,7 @@ TEST_F(CollectionTest, Feature_Query_Validate) {
 
   {
     VectorQuery query;
-    query.topk_ = 100000;
+    query.topk_ = 1024;
     query.field_name_ = field_name;
 
     auto field_scheama = schema->get_vector_field(field_name);
