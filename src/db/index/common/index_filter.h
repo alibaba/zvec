2a1af535efad26648d974670ec9c624d1d45792e
Jianning Wang <wangjianning.wjn@alibaba-inc.com>
2026-04-29 20:01:41 +0800

    index filter don't provide default count_filtered_in_range

--- src/db/index/common/index_filter.h
+++ src/db/index/common/index_filter.h
@@ -41,16 +41,8 @@ class IndexFilter {
 
   /**
    * Count how many items in [start, start+count) are filtered.
-   * Default iterates is_filtered(); subclasses may override for O(1)
-   * (e.g. via Roaring bitmap range_cardinality).
    */
-  virtual size_t count_filtered_in_range(uint64_t start, size_t count) const {
-    size_t filtered = 0;
-    for (size_t i = 0; i < count; i++) {
-      if (is_filtered(start + i)) filtered++;
-    }
-    return filtered;
-  }
+  virtual size_t count_filtered_in_range(uint64_t start, size_t count) const = 0;
 };
 
 class EasyIndexFilter : public IndexFilter {
@@ -82,6 +74,14 @@ class EasyIndexFilter : public IndexFilter {
    */
   bool is_filtered(uint64_t id) const override {
     return filter_func_(id);
+  }
+
+  size_t count_filtered_in_range(uint64_t start, size_t count) const override {
+    size_t filtered = 0;
+    for (size_t i = 0; i < count; i++) {
+      if (is_filtered(start + i)) filtered++;
+    }
+    return filtered;
   }
 
  private:
