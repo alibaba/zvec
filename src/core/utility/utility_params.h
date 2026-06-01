7da12289a9c01d0ea22ad2c33a9421c1b7bbaf52
Zefeng Yin <yinzefeng.yzf@alibaba-inc.com>
2026-06-01 21:29:24 +0800

    buffer read storage

--- src/core/utility/utility_params.h
+++ src/core/utility/utility_params.h
@@ -60,6 +60,14 @@ static const std::string MMAPFILE_READ_STORAGE_HEADER_OFFSET =
 static const std::string MMAPFILE_READ_STORAGE_FOOTER_OFFSET =
     "proxima.mmap_file.container.footer_offset";
 
+//! BufferReadStorage (read-only storage backed by VecBufferPool)
+static const std::string BUFFER_READ_STORAGE_CHECKSUM_VALIDATION =
+    "proxima.buffer.read_storage.checksum_validation";
+static const std::string BUFFER_READ_STORAGE_HEADER_OFFSET =
+    "proxima.buffer.read_storage.header_offset";
+static const std::string BUFFER_READ_STORAGE_FOOTER_OFFSET =
+    "proxima.buffer.read_storage.footer_offset";
+
 //! MMapFileStorage
 static const std::string MMAPFILE_STORAGE_MEMORY_LOCKED =
     "proxima.mmap_file.storage.memory_locked";
