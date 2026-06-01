2af1471cf2702bc22e28413fcd8fb4ba683f845a
Qinren Zhou <zhouqinren.zqr@alibaba-inc.com>
2026-06-01 19:53:31 +0800

    fix

--- src/db/index/column/vector_column/combined_vector_column_indexer.h
+++ src/db/index/column/vector_column/combined_vector_column_indexer.h
@@ -38,7 +38,6 @@ class CombinedVectorColumnIndexer {
       const vector_column_params::VectorData &vector_data,
       const vector_column_params::QueryParams &query_params);
 
-  // segment_doc_id is segment-local, not block-local.
   virtual Result<vector_column_params::VectorDataBuffer> Fetch(
       uint32_t segment_doc_id) const;
 
