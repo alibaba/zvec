6d8f817d857f4a456635779e4ddb8f6d9106f420
Zefeng Yin <yinzefeng.yzf@alibaba-inc.com>
2026-08-05 20:05:15 +0800

    clang format

--- src/ailego/buffer/vector_page_table.cc
+++ src/ailego/buffer/vector_page_table.cc
@@ -20,7 +20,6 @@
 #include <ailego/utility/memory_helper.h>
 #include <zvec/ailego/buffer/vector_page_table.h>
 #include <zvec/ailego/logger/logger.h>
-#include <zvec/ailego/logger/logger.h>
 
 #if defined(_MSC_VER)
 #ifndef NOMINMAX
