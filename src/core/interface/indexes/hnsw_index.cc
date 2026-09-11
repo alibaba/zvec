7d0c1f489b61c998e4186e27f1f1813031d31e77
luoxiaojian <lxj193371@alibaba-inc.com>
2026-09-11 15:07:01 +0800

    Honor refine scale factor for graph indexes

--- src/core/interface/indexes/hnsw_index.cc
+++ src/core/interface/indexes/hnsw_index.cc
@@ -190,14 +190,4 @@ int HNSWIndex::_prepare_for_search(
   return 0;
 }
 
-int HNSWIndex::_get_coarse_search_topk(
-    const BaseIndexQueryParam::Pointer &search_param) {
-  const auto &hnsw_search_param =
-      std::dynamic_pointer_cast<HNSWQueryParam>(search_param);
-
-  // scale_factor doesn't take effect for hnsw.
-  auto ret = std::max(search_param->topk, hnsw_search_param->ef_search);
-  return ret;
-}
-
 }  // namespace zvec::core_interface
