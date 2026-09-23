0f6f2a325690de16577df78d4cfbdb69253343b0
Zefeng Yin <yinzefeng.yzf@alibaba-inc.com>
2026-09-23 15:53:19 +0800

    feat(ivf): stage training and build vectors in buffer storage

--- src/include/zvec/core/interface/index.h
+++ src/include/zvec/core/interface/index.h
@@ -340,7 +340,8 @@ class ZVEC_CORE_API IVFIndex : public Index {
   BuildStage build_stage_{BuildStage::kCollecting};
   IVFIndexParam param_{};
   std::mutex mutex_{};
-  std::vector<std::pair<uint64_t, std::string>> doc_cache_;
+  using DocCache = std::vector<std::pair<uint64_t, std::string>>;
+  std::shared_ptr<DocCache> doc_cache_{std::make_shared<DocCache>()};
   core::IndexHolder::Pointer holder_{};
   std::string file_path_;
 };
